e63661ec15b57c6783fac7a330c57e5575e2dc00
Denis Zemlianoi <dzemlianoi@gmail.com>
2016-02-03 16:55:34 +0200

    Переменные, они же запросы - вынесены в конфиги

--- templates/config.php
+++ templates/config.php
@@ -3,3 +3,6 @@ const HOST="localhost";
 const USER_DB="foodtourist";
 const PASS_DB="denis";
 const DB_NAME="foodtourist";
+const ALL_CATEGORIES="SELECT * FROM cathegories";
+const NEWINGS_SHOW="SELECT * FROM goods WHERE is_new=1 LIMIT 4";
+const HOTS_SHOW="SELECT * FROM goods WHERE is_hot=1 LIMIT 4";
