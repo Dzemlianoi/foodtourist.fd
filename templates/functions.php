359dcd4efe1884d6e8b3f22ab8343e2f848ff85e
Denis Zemlianoi <dzemlianoi@gmail.com>
2016-02-03 16:54:10 +0200

    Адаптированы функции get и push категорий hot и new

--- templates/functions.php
+++ templates/functions.php
@@ -1,79 +1,61 @@
 <?php
 include_once ('config.php');
-function get_cathegory()
-{
-    $db = mysqli_connect(HOST, USER_DB, PASS_DB, DB_NAME);
-    if ($db) {
-        //charset-----------------------------------
-        mysqli_query($db, "SET NAMES 'utf8'");
-        mysqli_query($db, "SET CHARACTER SET 'utf8'");
-        mysqli_query($db, "SET SESSION collation_connection = 'utf8_general_ci'");
-        //------------------------------------------
+$db_connect=NULL;
 
-        $query = "SELECT * FROM cathegories";
-        $result = mysqli_query($db, $query);
-        if ($result) {
-            $cathegories = array();
-            while ($cathegory = mysqli_fetch_assoc($result)) {
-                array_push($cathegories, $cathegory);
-            }
-        } else {
-            //тут необходимо записать логи
-            exit("can't get categories");
-        }
-        mysqli_close($db);
-    } else {
-        //тут необходимо записать логи
-        exit('no db connection');
+//Достаем базу
+function get_db(){
+    if (empty($db_connect)){
+        $db_connect= mysqli_connect(HOST, USER_DB, PASS_DB, DB_NAME);
     }
-    return $cathegories;
+    return $db_connect;
+}
+//Устанавливаем кодировку
+function set_mains(){
+    $db=get_db();
+        if ($db) {
+            //charset-----------------------------------
+            mysqli_query($db, "SET NAMES 'utf8'");
+            mysqli_query($db, "SET CHARACTER SET 'utf8'");
+            mysqli_query($db, "SET SESSION collation_connection = 'utf8_general_ci'");
+            //------------------------------------------
+        }
+    return $db;
 
 }
+//Достаем нужные данные и пушим их в массив
+function get_data($query) {
+    $db=set_mains();
+    $result = mysqli_query($db, $query);
+    if ($result) {
+        $data_array = array();
+        while ($data = mysqli_fetch_assoc($result)) {
+            array_push($data_array, $data);
+        }
+    } else {
+        exit("can't get data");
+    }
+    mysqli_close($db);
+    return $data_array;
+}
 
+//Выводим категории
 function push_cathegories(){
-    foreach(get_cathegory() as $cat){
-        $push_categories.='<li class="col-md-2"><a href="#">'.$cat['name'].'</a></li>';
+    foreach(get_data(ALL_CATEGORIES) as $cat){
+        $push_categories.='<li class="col-md-2"><a href="http://foodtourist.fd/category='.$cat['id'].'">'.$cat['name']
+        .'</a></li>';
     }
     return $push_categories;
 }
-
-function get_hot($query){
-    $db = mysqli_connect(HOST, USER_DB, PASS_DB, DB_NAME);
-    if ($db) {
-        //charset-----------------------------------
-        mysqli_query($db, "SET NAMES 'utf8'");
-        mysqli_query($db, "SET CHARACTER SET 'utf8'");
-        mysqli_query($db, "SET SESSION collation_connection = 'utf8_general_ci'");
-        //------------------------------------------
-
-        $result = mysqli_query($db, $query);
-        if ($result) {
-            $hots = array();
-            while ($hot = mysqli_fetch_assoc($result)) {
-                array_push($hots, $hot);
-            }
-        } else {
-            //тут необходимо записать логи
-            exit("can't get categories");
-        }
-        mysqli_close($db);
-    } else {
-        //тут необходимо записать логи
-        exit('no db connection');
-    }
-    return $hots;
-
-}
-
-function push_hots(){
-    foreach(get_hot("SELECT * FROM goods WHERE is_hot=1 LIMIT 4") as $hot){
+//Выводим хот предложения
+function push_hots_or_newings($TYPE, $text){
+    foreach(get_data($TYPE) as $hot){
         $push_hot.='<div class="special-element">
-            <div class="sale-sprite"></div>
+            <div class="'.$text.'"></div>
             <img class="special-image" src="../images/goods/pizza/'.$hot["picture_name"].'" alt=""/>
             <div class="name-box">
                 <span class="name">'.$hot["name"].'</span>
             </div>
-            <span class="price">'.$hot["price"].'</span>
+            <span class="price">'.$hot["price"].' грн</span>
             <form id="'.$hot["id"].'" method="post">
                 <input type="number" value=0 name="quantity">
                 <input type="submit" value="Заказать">
@@ -82,22 +64,3 @@ function push_hots(){
     }
     return $push_hot;
 }
-function push_newings(){
-    foreach(get_hot("SELECT * FROM goods WHERE is_new=1 LIMIT 4") as $newing){
-        $push_new.='<div class="special-element">
-            <div class="new-sprite"></div>
-            <img class="special-image" src="../images/goods/pizza/'.$newing["picture_name"].'" alt=""/>
-            <div class="name-box">
-                <span class="name">'.$newing["name"].'</span>
-            </div>
-            <span class="price">'.$newing["price"].'</span>
-            <form id="'.$newing["id"].'" method="post">
-                <input type="number" value=0 name="quantity">
-                <input type="submit" value="Заказать">
-            </form>
-        </div>';
-    }
-    return $push_new;
-}
-
-
