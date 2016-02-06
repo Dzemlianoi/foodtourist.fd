1717aae08963e6a9058e454bc8db22d064934684
Denis Zemlianoi <dzemlianoi@gmail.com>
2016-02-06 11:34:03 +0200

    Определена первоночальная функция для пуша хлебных крошек

--- templates/functions.php
+++ templates/functions.php
@@ -46,6 +46,7 @@ function push_cathegories(){
             $selector='';
             if($_GET['category']===$cat['id']){
                 $selector=' active-cathegory';
+                $cat['name'];
 //            }
         };
         $push_categories.=
@@ -77,21 +78,52 @@ function push_hots_or_newings($TYPE, $text){
 
 
 function get_request_param($param){
-    if (!empty($_GET[$param])){
+    if ((isset($_GET[$param]))&&(!empty($_GET[$param]))){
         return $_GET[$param];
     }else{
         return false;
     }
 }
-function get_content_cathegory()
-{
-    if (get_request_param('category')) {
-        foreach (get_data("SELECT 'id', 'name' FROM cathegories") as $id => $name) {
-            if ($id == get_request_param('category')) {
-                return $result == $id;
-            } else {
-                return 'категория не найдена';
+
+function get_current_cat_name(){
+    $cat_id=get_request_param('category');
+    if ($cat_id){
+        foreach (get_data(ALL_CATEGORIES) as $cat){
+            if ($cat['id']===$cat_id){
+                return $cat['name'];
+                break;
             }
         }
+        return 'Текущей категории нет';
     }
+    return 'Не выбрана категория или категории не существует';
+
 }
+
+function recognize_php_file_by_url(){
+    $server_path=pathinfo($_SERVER['PHP_SELF'],PATHINFO_FILENAME);
+    return $server_path;
+}
+function breadcrumbs_push(){
+    $param=recognize_php_file_by_url();
+    switch ($param){
+        case 'categories':
+            $push_bread='
+            <a href="index.php">
+                <span class="post-bread marg-r-10">Главная </span>
+            </a>
+                <span class="line-bread marg-r-10">→</span>
+                <span class="chosen-bread marg-r-10">'.get_current_cat_name().'</span>';
+            break;
+    }
+    echo $push_bread;
+
+
+}
+
+
+//if ((isset($_GET['cathegory'])) && (!isset($_GET['good']))){
+//        $current_cathegory=$_GET('cathegory');
+//        $breadcrumbs=<<<QQ
+//        <a href="index.php"><span class="post-bread">Главная</span></a> → <span class="chosen-bread">
+//            {get_data("SELECT name FROM cathegories WHERE id=${current_cathegory}"}</span>
