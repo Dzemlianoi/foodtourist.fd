ff105db77939483d55bbc63a3850301a5f6bfa54
Denis Zemlianoi <dzemlianoi@gmail.com>
2016-02-06 11:35:27 +0200

    Добавлена полная страница категорий

--- categories.php
+++ categories.php
@@ -1,5 +1,7 @@
 <?php
 include_once "templates/header.php";
+include_once "templates/breadcrumbs.php";
+include_once "templates/categories-template.php";
 include_once "templates/seo_info.php";
 include_once "templates/footer.php";
 ?>
