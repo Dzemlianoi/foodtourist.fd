fbd42888e545f24af23068a14ba1af0647bb769d
Denis Zemlianoi <dzemlianoi@gmail.com>
2016-02-06 11:15:32 +0200

    Перемещаем все вендоры - сss+js

--- js/js.js
+++ js/js.js
@@ -2,7 +2,7 @@
  * Created by Жира on 01.02.2016.
  */
 $(document).ready(function() {
-    $("#carousel").owlCarousel();
+    $("#carousel").owlCarousel()
 });
 
 $("#carousel").owlCarousel({
@@ -11,4 +11,44 @@ items:1,
     loop:false,
     singleitem:true,
     autoplay:true
+});
+
+//var slider = document.getElementById('price-filter');
+//
+//noUiSlider.create(slider, {
+//    start: [20, 80],
+//    connect: true,
+//    range: {
+//        'min': 0,
+//        'max': 100
+//    }
+//});
+var slider = document.getElementById('price');
+
+noUiSlider.create(slider, {
+    start: [0, 100],
+    connect: true,
+    range: {
+        'min': 0,
+        'max': 100
+    }
+});
+var valueInput = document.getElementById('value-min'),
+    valueMax = document.getElementById('value-max');
+
+// When the slider value changes, update the input and span
+slider.noUiSlider.on('update', function( values, handle ) {
+    if ( handle ) {
+        valueMax.value = values[handle];
+    } else {
+        valueInput.value = values[handle];
+    }
+});
+
+// When the input changes, set the slider value
+valueInput.addEventListener('change', function(){
+    slider.noUiSlider.set([null, this.value]);
+});
+valueMax.addEventListener('change', function(){
+    slider.noUiSlider.set([null, this.value]);
 });
