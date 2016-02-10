45215b717e9da686a8c41c1dca059a7e371c5a58
Denis Zemlianoi <dzemlianoi@gmail.com>
2016-02-10 16:41:59 +0200

    Добавлены константы-запросы:  а) получение названия категории по ее id;  б) получение максимальной цены в категории  в) получение минимальной цены в категории

--- templates/config.php
+++ templates/config.php
@@ -6,3 +6,17 @@ const DB_NAME="foodtourist";
 const ALL_CATEGORIES="SELECT * FROM cathegories";
 const NEWINGS_SHOW="SELECT * FROM goods WHERE is_new=1 LIMIT 4";
 const HOTS_SHOW="SELECT * FROM goods WHERE is_hot=1 LIMIT 4";
+const SELECT_FILTER_BY_CAT_ID="
+SELECT filters.id, cathegories.name, filters.name,filters.param_url
+FROM cathegories, filters, filters_to_cathegories
+WHERE cathegories.id = filters_to_cathegories.category_id
+AND filters.id = filters_to_cathegories.filter_id
+AND cathegories.id =";
+const MAX_PRICE_FROM_CATS="SELECT MAX( goods.price )
+FROM cathegories, goods
+WHERE cathegories.id = goods.cathegory_id
+AND cathegories.id =";
+const MIN_PRICE_FROM_CATS="SELECT MIN( goods.price )
+FROM cathegories, goods
+WHERE cathegories.id = goods.cathegory_id
+AND cathegories.id =";
