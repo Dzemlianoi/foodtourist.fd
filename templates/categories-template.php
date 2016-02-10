d64b5a1961153b8bca7c142eefdb8eb3777b5b5b
Denis Zemlianoi <dzemlianoi@gmail.com>
2016-02-10 16:50:35 +0200

    Добавлено тело страницы категорий

--- templates/categories-template.php
+++ templates/categories-template.php
@@ -1,49 +1,101 @@
-<div class="container-fluid full-cat" xmlns="http://www.w3.org/1999/html">
-    <div class="col-md-2 container filter-cat">
+<div class="container-fluid full-cat" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html">
+    <div class="col-md-2 container filter-cat inline">
         <div class="row">
 <!--            Прайс-фильтр-->
             <div class="price-filter filter-block for-submit">
                 <span class="filter-name">Цена</span>
-                <form method="get" name="filter">
+                <form method="get" name="filter" action="categories.php?category=<?php echo$_GET['category']?>">
                     <div id="price">
                     </div>
                     <div class="price-controllers">
                         <input type="text" class="fil-from-to" name="min" id="value-min"/>
                         <input type="text" class="fil-from-to" name="max" id="value-max"/>
                     </div>
-                    <input type="submit" class="accept-filter" value="Фильтр">
-                </form>
             </div>
-            <!--             Фильтр бокса-->
-            <div class="filter-block box-filter for-submit">
-                <span class="filter-name">Наличие упаковки</span>
-                <form method="get" name="box">
-                    <input type="checkbox" id="box1" value="1"> <label class="fil-name-chk"
-                                                                       for="box1">Да</label><br/>
-                    <input type="checkbox" id="box2" value="0"> <label class="last-fil-name-chk fil-name-chk"
-                                                                       for="box2">Нет</label><br/>
-                    <input type="submit" class="accept-filter" value="Фильтр">
-                </form>
-            </div>
-            <!--            Фильтр соусов-->
-            <div class="filter-block sous-filter for-submit">
-                <span class="filter-name">Наличие соуса</span>
-                    <input type="checkbox" id="sous1" value="1"> <label class="fil-name-chk"
-                                                                       for="sous1">Да</label><br/>
-                    <input type="checkbox" id="sous2" value="0"> <label class="last-fil-name-chk fil-name-chk"
-                                                                       for="sous2">Нет</label><br/>
-                    <input type="submit" class="accept-filter" value="Фильтр">
-            </div>
-            <!--            Фильтр бокса-->
-            <div class="filter-block box-filter for-submit">
-                <span class="filter-name">Вегетарианские</span>
-                    <input type="checkbox" id="box1" value="1"> <label class="fil-name-chk"
-                                                                       for="box1">Да</label><br/>
-                    <input type="checkbox" id="box2" value="0"> <label class="last-fil-name-chk fil-name-chk"
-                                                                       for="box2">Нет</label><br/>
-            </div>
+            <?php echo get_filters_by_cat_id() ?>
+            <input type="submit" class="submit-filter" value="Фильтр">
+
         </div>
     </div>
+    <div class="col-md-8 catalog container  inline">
+        <div class="row catalog-cat-name ">
+             <span class="cat-name-filter">Пицца</span>
+             <span class="numb-goods">(32 товара)</span>
+        </div>
+        <div class="cat-fil-schema container">
+            <div class="inline type-show-box type-chosen">
+                <span class="show-filters">Показать:</span>
+                <select class="form-control choose-filter">
+                    <option>От самых дорогих к самым дешевым</option>
+                    <option>От самых дешевых к самым дорогим</option>
+                    <option>По названию</option>
+                    <option>Только акции</option>
+                    <option>Только новинки</option>
+                </select>
+            </div>
+            <div class="inline type-show-box type-t-or-l">
+                <span class="type-list">Отображать:</span>
+                <label for="type-of-show1">Листом</label>
+                <input type ="radio" id="type-of-show1" name="type-of-show" value="list" checked>
+                <label for="type-of-show1">Плиткой</label>
+                <input type ="radio" id="type-of-show2" name="type-of-show" value="table">
+                <input type="hidden" name="category" value="<?php echo $_GET['category'] ?>">
+            </div>
+            <input type="submit" class="submit-filter" value="Фильтр">
+        </div>
+    </div>
+    <div class="goods-cat container">
+        <div class="cat-item container col-md-9">
+            <div  class="cat-it-img-blk inline">
+                <img class="cat-it-image" src="../images/goods/pizza/berluskoni.png"/>
+            </div>
+            <div class="cat-it-info inline">
+                <span class="cat-it-name">Берлускони</span>
+                <span class="cat-it-price">Цена:  101 грн</span>
+                <input type="number" min="0" value=0 name="quantity">
+                <input type="submit" value="Заказать" name="gooda">
+                <p class="cat-short-desc">Состав пиццы: соус пикант,сыр моцарелла, свинина маринованная, телятина
+                    маринованная,
+                    куриное
+                    филе, лук, помидоры, петрушка, укроп</p>
+            </div>
+        </div>
+        <div class="cat-item container col-md-9">
+            <div  class="cat-it-img-blk inline">
+                <img class="cat-it-image" src="../images/goods/pizza/berluskoni.png"/>
+            </div>
+            <div class="cat-it-info inline">
+                <span class="cat-it-name">Берлускони</span>
+                <span class="cat-it-price">Цена:  101 грн</span>
+                <input type="number" min="0" value=0 name="quantity">
+                <input type="submit" value="Заказать" name="goodb">
+                <p class="cat-short-desc">Состав пиццы: соус пикант,сыр моцарелла, свинина маринованная, телятина
+                    маринованная,
+                    куриное
+                    филе, лук, помидоры, петрушка, укроп</p>
+            </div>
+        </div>
+        <div class="cat-item container col-md-9">
+            <div  class="cat-it-img-blk inline">
+                <img class="cat-it-image" src="../images/goods/pizza/berluskoni.png"/>
+            </div>
+            <div class="cat-it-info inline">
+                <span class="cat-it-name">Берлускони</span>
+                <span class="cat-it-price">Цена:  101 грн</span>
+                <input type="number" min="0" value=0 name="quantity">
+                <input type="submit" value="Заказать" name="goodv">
+                <p class="cat-short-desc">Состав пиццы: соус пикант,сыр моцарелла, свинина маринованная, телятина
+                    маринованная,
+                    куриное
+                    филе, лук, помидоры, петрушка, укроп</p>
+            </div>
+        </div>
+        </form>
+    </div>
+
 </div>
+
+
+
 
 
